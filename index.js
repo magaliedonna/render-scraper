7e8f17c7b45b3ea2e649c42c39e9c96760d9778a
Magalouse <gamerzpot12@gmail.com>
2024-08-03 14:07:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/tiktok", (req, res) => {
   scrapeLogic(res, req);
