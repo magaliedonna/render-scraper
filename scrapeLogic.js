fe27e6b064f996284465d65ee8c049cd2a733a12
Magalouse <gamerzpot12@gmail.com>
2024-08-03 14:56:39 +0200

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const scrapeLogic = async (res, req) => {
   const url = req.query.url;
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -16,27 +17,31 @@ const scrapeLogic = async (res, req) => {
         : puppeteer.executablePath(),
   });
   try {
-        const page = await browser.newPage();
+        let page = await browser.newPage();
+
+        
         await page.goto(url);
-        await page.setViewport({ width: 850, height: 1000 });
-    
-        await page.waitForSelector('svg[class="verify-bar-close--icon"]');
-        await page.click('svg[class="verify-bar-close--icon"]');
+        await page.setViewport({ width: 1280, height: 720 });
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36');
+;
+        /*await page.waitForSelector('svg[class="verify-bar-close--icon"]', { visible: true });
+        await page.click('svg[class="verify-bar-close--icon"]', { delay: 100 });*/
 
        const infos = await page.evaluate(() => {
-            const img = document.querySelector('div[data-e2e="user-avatar"] span img').getAttribute('src');
-            const username = document.querySelector('h1[data-e2e="user-title"]').innerText;
-            const bio = document.querySelector('h2[data-e2e="user-bio"]').innerText;
-            const like = document.querySelector('strong[data-e2e="likes-count"]').innerText;
-            const followers = document.querySelector('strong[data-e2e="followers-count"]').innerText;;
+            let img = document.querySelector('div[data-e2e="user-avatar"] span img').getAttribute('src');
+            let username = document.querySelector('h1[data-e2e="user-title"]').innerText;
+            let bio = document.querySelector('h2[data-e2e="user-bio"]').innerText;
+            let like = document.querySelector('strong[data-e2e="likes-count"]').innerText;
+            let followers = document.querySelector('strong[data-e2e="followers-count"]').innerText;;
         return{img, username, bio, like, followers};
 
        });
+
        res.send({imageProfile: infos.img, username: infos.username, bio: infos.bio, likes: infos.like, followers: infos.followers});
     
     } catch (e) {
     console.error(e);
-    res.send("Hum une erreur");
+    res.send("<h1>Hum une erreur</h1>");
   } finally {
     await browser.close();
   }
